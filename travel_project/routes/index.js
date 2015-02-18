be12a25bf67a3c37036efc3aedaf52a34237a202
kimmihyeon <alqhd2712@naver.com>
2015-02-19 01:10:52 +0900

    첫페이지, main생성

--- travel_project/routes/index.js
+++ travel_project/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/main', function(req,res){
+	res.render('main', { title: 'Express' });
+})
+
 module.exports = router;
